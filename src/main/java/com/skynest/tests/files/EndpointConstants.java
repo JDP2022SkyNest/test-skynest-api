668438d67ba85f53026e81145527ae3d038696e9
Hristina Zaharieva <hristina.zaharieva@htecgroup.com>
2022-05-31 15:38:36 +0200

    First Commit

---
+++ src/main/java/com/skynest/tests/files/EndpointConstants.java
@@ -0,0 +1,6 @@
+package com.skynest.tests.files;
+
+public class EndpointConstants {
+    public static final String url = "http://localhost:8080";
+    public static final String registerUrl="/users/register";
+}
