360ff34a78dbd13fb4abb47d7e75a62c78eed7e5
Nemanja <nemanja.mihajlovic@htecgroup.com>
2022-06-15 10:56:57 +0200

    After errors

---
+++ src/main/java/com/skynest/tests/files/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.skynest.tests.files;
+
+public class ApiConstants {
+
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String REGISTER_ENDPOINT = "/users/register";
+}
